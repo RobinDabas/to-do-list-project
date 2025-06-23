dde7330f0740c51367d3ae6eb0bc41ddfd75a9cd
ROBERTO D <robindabas@hotmail.com>
2025-06-23 15:00:57 -0400

    learning how use js with input buttons

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function addTask() {
-    const taskInput = document.getElementById('taskInput');
-    const taskText = taskInput.value.trim();
-
-    if(taskText !== '') {
-    const li = document.createElement('li');
-    li.textContent = taskText;
-
-    document.getElementById('taskList').appendChild(li);
-    taskInput.value = '';
+  const taskInput = document.getElementById('taskInput');
+  const taskText = taskInput.value.trim;  
+  if(taskText !== ''){
+    console.log('Valid task:', taskText)
+  } else {
+    console.log('No task entered!')
   }
 }
-
